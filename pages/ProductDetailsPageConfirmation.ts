328b1ae189e694d7537433e948e24511b15567b5
Dmitriy2509 <sakovetsdmytryi@gmail.com>
2026-01-18 19:37:19 +0200

    change locators

--- pages/ProductDetailsPageConfirmation.ts
+++ pages/ProductDetailsPageConfirmation.ts
@@ -2,11 +2,12 @@ import { Locator } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class ProductDetailsPageConfirmation extends BasePage {
-  readonly closeModalIcon: Locator = this.page.locator(
-    '//*[@id="blockcart-modal"]//*[@aria-label="Close"]'
-  );
+  readonly modal: Locator = this.page.locator("#blockcart-modal");
+  readonly closeModalIcon: Locator = this.modal.locator('[aria-label="Close"]');
 
   async clickCloseModalIcon() {
+    await this.modal.waitFor({ state: 'visible' });
+    await this.closeModalIcon.waitFor({ state: 'visible' });
     await this.closeModalIcon.click();
   }
 }
