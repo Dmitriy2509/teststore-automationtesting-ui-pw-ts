5a61b7a9ee579bf14d28149c67523ac4d4c94f63
Dmitriy2509 <sakovetsdmytryi@gmail.com>
2026-01-18 16:40:11 +0200

    removed waitFor

--- pages/ProductDetailsPageConfirmation.ts
+++ pages/ProductDetailsPageConfirmation.ts
@@ -2,13 +2,11 @@ import { Locator } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class ProductDetailsPageConfirmation extends BasePage {
-  readonly confirmationForm: Locator = this.page.locator("#blockcart-modal");
   readonly proceedToCheckoutBtn: Locator = this.page.locator(
     ".cart-content-btn a"
   );
 
   async clickProceedToCheckoutBtn() {
-    await this.confirmationForm.waitFor({ state: 'attached' });
     await this.proceedToCheckoutBtn.click();
   }
 }
