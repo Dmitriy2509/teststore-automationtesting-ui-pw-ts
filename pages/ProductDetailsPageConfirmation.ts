770f8b985755751eef9f4f3febadbe16c5007569
Dmitriy2509 <sakovetsdmytryi@gmail.com>
2026-01-18 18:50:40 +0200

    updated method for checkout

--- pages/ProductDetailsPageConfirmation.ts
+++ pages/ProductDetailsPageConfirmation.ts
@@ -1,13 +1,13 @@
-import { expect, Locator } from "@playwright/test";
+import { Locator } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class ProductDetailsPageConfirmation extends BasePage {
   readonly proceedToCheckoutBtn: Locator = this.page.locator(
-    ".cart-content-btn .btn-secondary"
+    '//*[@id="blockcart-modal"]//*[@aria-label="Close"]'
   );
 
   async clickProceedToCheckoutBtn() {
-    await this.proceedToCheckoutBtn.click({force: true});
+    await this.proceedToCheckoutBtn.click();
     await this.page.locator("#_desktop_cart").click();
   }
 }
